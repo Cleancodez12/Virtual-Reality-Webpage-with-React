0b1add065a473be05b83a5b555e0560f59be7df3
Peter Aina <ainapeter123@gmail.com>
2025-11-29 21:38:28 +0000

    created testimonial section with proper styling.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -33,8 +33,10 @@ const Pricing = () => {
               </ul>
               <a
                 href="#"
-                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border-orange-900 rounded-lg transition duration-200"
-              ></a>
+                className="inline-flex justify-center border items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border-orange-900 rounded-lg transition duration-200"
+              >
+                Subscribe
+              </a>
             </div>
           </div>
         ))}
